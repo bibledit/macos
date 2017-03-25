00dfa23166b136dbe158b5acb8155881e8f312c0
Teus Benschop <teusjannette@gmail.com>
2017-03-25 20:22:50 +0100

    Can upload file
    This fixes https://github.com/bibledit/bibledit/issues/504.

--- osx/AppDelegate.m
+++ osx/AppDelegate.m
@@ -120,4 +120,24 @@
 }
 
 
+- (void)webView:(WebView *)sender runOpenPanelForFileButtonWithResultListener:(id < WebOpenPanelResultListener >)resultListener
+{
+    // Create the file open dialog.
+    NSOpenPanel* openDlg = [NSOpenPanel openPanel];
+    
+    // Enable the selection of files in the dialog.
+    [openDlg setCanChooseFiles:YES];
+    
+    // Enable the selection of directories in the dialog.
+    [openDlg setCanChooseDirectories:NO];
+    
+    // Run it.
+    if ( [openDlg runModal] == NSModalResponseOK )
+    {
+        NSArray* files = [[openDlg URLs]valueForKey:@"relativePath"];
+        [resultListener chooseFilenames:files];
+    }
+}
+
+
 @end
