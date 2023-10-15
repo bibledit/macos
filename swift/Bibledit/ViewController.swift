422d14d76686302bb9f7ba2efc5464e58cbf1a49
Teus Benschop <teusjannette@gmail.com>
2023-10-15 19:50:36 +0200

    Embed a webview
    https://github.com/bibledit/cloud/issues/920

--- swift/Bibledit/ViewController.swift
+++ swift/Bibledit/ViewController.swift
@@ -16,22 +16,37 @@
  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
+// The initial steps to embed a webview were taken from this:
+// https://stackoverflow.com/questions/60082417/how-do-i-create-a-wkwebview-app-for-macos
+
 import Cocoa
+import WebKit
 
-class ViewController: NSViewController {
-
+class ViewController: NSViewController, WKUIDelegate
+{
+    var web_view: WKWebView!
+    
+    override func loadView()
+    {
+        let web_configuration = WKWebViewConfiguration ()
+        web_configuration.preferences.setValue(true, forKey: "allowFileAccessFromFileURLs")
+        web_view = WKWebView (frame: CGRect(x:0, y:0, width:800, height:600), configuration:web_configuration)
+        web_view.uiDelegate = self
+        view = web_view
+    }
+    
     override func viewDidLoad() {
+        print ("view did load")
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        let url = Bundle.main.url ( forResource: "index",
+                                    withExtension: "html",
+                                    subdirectory: "webroot")
+        print (url!)
+        let path = url!.deletingLastPathComponent();
+        print (path)
+        self.web_view.loadFileURL ( url!, allowingReadAccessTo: path)
+        self.view = web_view
+        NSApp.activate(ignoringOtherApps: true)
+        NSApp.windows.first?.orderFrontRegardless()
     }
-
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
-        }
-    }
-
-
 }
-
