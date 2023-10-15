0a307d303cb038f4ed32f6b94bd497f21e0f6f79
Teus Benschop <teusjannette@gmail.com>
2023-10-15 17:22:44 +0200

    Create a new macOS app written in Swift.
    https://github.com/bibledit/cloud/issues/920

---
+++ swift/Bibledit/BibleditApp.swift
@@ -0,0 +1,17 @@
+//
+//  BibleditApp.swift
+//  Bibledit
+//
+//  Created by Teus Benschop on 15/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct BibleditApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
